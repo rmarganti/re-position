7b2ece09794ece4539e1747705d4798c416f6db0
Ryan Marganti <rmarganti@gannett.com>
2019-03-11 21:48:22 -0400

    jest, etc. dependencies updates.

--- testingConfig.ts
+++ testingConfig.ts
@@ -1,4 +1,4 @@
 import { configure as configureEnzyme } from 'enzyme';
-import React16Adapter from 'enzyme-adapter-react-16';
+import * as React16Adapter from 'enzyme-adapter-react-16';
 
 configureEnzyme({ adapter: new React16Adapter() });
